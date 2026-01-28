f25174a28427b0e1b7395f30cfb2c6d753b1c99e
chanasorn <Chanasorn1013@gmail.com>
2026-01-28 19:24:08 +0700

    Update page.tsx

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -3,7 +3,10 @@ import LoginForm from "./LoginForm";
 export default function LoginPage() {
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-100">
-      <LoginForm />
+      <h1 className="text-2xl font-bold text-gray-950 mx-auto">LOGIN ก่อนนะจ๊ะ!</h1>
+      <div>
+        <LoginForm />
+      </div>
     </div>
   );
 }
