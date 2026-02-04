c47a39d29f1d41c4861f1dc72da3a31fef4b84c9
ZordMunDee <zeroznice.nice@gmail.com>
2026-02-04 11:13:49 +0700

    flex: ux

--- components/attendance/RoomHeader.tsx
+++ components/attendance/RoomHeader.tsx
@@ -5,10 +5,10 @@ type Props = {
 export function RoomHeader({ room }: Props) {
   return (
     <div>
-      <p className="text-sm font-semibold text-orange-500">
+      <p className="text-lg font-semibold text-orange-500">
         CURRENT ROOM
         </p>
-      <h2 className="text-5xl font-extrabold">
+      <h2 className="text-8xl font-extrabold">
         {room}
         </h2>
     </div>
