010d61023bca91f82c31ccdda4455bf927acf69a
chanasorn <Chanasorn1013@gmail.com>
2026-03-13 02:41:30 +0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,13 @@
 "use client";
 import { LoadingSpinner } from "@/components/LoadingSpinner";
-import { redirect, useRouter } from "next/navigation";
-import { useEffect } from "react";
+// import { redirect, useRouter } from "next/navigation";
+// import { useEffect } from "react";
 
 // import { redirect } from "next/navigation";
 
 export default function Home() {
-  const router = useRouter();
-  useEffect(() => {router.back();}, []);
+  // const router = useRouter();
+  // useEffect(() => {router.back();}, []);
   // redirect("/teacher-login"); //เปลี่ยนเป็นหน้า Login แทน (เอาไว้หลีกเลี่ยงปัญหาคนหลุดคอกลิกมา page นี้)
   //  loop คนหลุดคอกลิกมา page นี้
   return <div className="flex justify-center items-center h-screen"> <LoadingSpinner /> </div>;
